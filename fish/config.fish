762a508265086e1fa6a93b8e029b6b9edc684be5
Paul <1mayanktomar@gmail.com>
2024-12-29 23:45:32 +0530

    feat: modular fish config

--- fish/config.fish
+++ fish/config.fish
@@ -105,13 +105,6 @@ alias c="clear"
 
 fish_ssh_agent
 
-# >>> mamba initialize >>>
-# !! Contents within this block are managed by 'mamba init' !!
-set -gx MAMBA_EXE "/home/paul/.local/bin/micromamba"
-set -gx MAMBA_ROOT_PREFIX "/home/paul/micromamba"
-$MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
-# <<< mamba initialize <<<
-
 function rawdog
   set -gx VCPKG_ROOT "$PWD/.vcpkg"
 
---
+++ fish/krill/micromamba/conf.d/init.fish
@@ -0,0 +1,6 @@
+# >>> mamba initialize >>>
+# !! Contents within this block are managed by 'mamba init' !!
+#set -gx MAMBA_EXE "/home/paul/.local/bin/micromamba"
+#set -gx MAMBA_ROOT_PREFIX "/home/paul/micromamba"
+#$MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
+# <<< mamba initialize <<<
